cb7db144aab67590c7553f6f1c46e1fae2dc40fc
Carl Arvidsson <calle.arvidssons@gmail.com>
2015-12-02 14:55:31 +0100

    Lab3 halvklar?!

---
+++ lab3.m
@@ -0,0 +1,32 @@
+% b)
+
+load lab3_extra/uppgift5b.mat
+
+pzchange(H1)
+hold on;
+pzchange(H2)
+
+%% c)
+
+load lab3_extra/uppgift5c.mat
+
+pzchange(H3)
+
+%% d)
+
+% ????
+
+%% e)
+
+[B, A] = butter(10, 2 * pi * 100, 'low', 's');
+pzchange(in(B, A, 's'))
+
+%%
+[D, C] = cheby1(5, 3, 2 * pi * 100, 'low', 's');
+pzchange(in(D, C, 's'))
+
+%% f)
+
+load lab3_extra/uppgift5f.mat
+
+pzchange(H5)
